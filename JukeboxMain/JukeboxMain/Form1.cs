0a63195fe6fe7fa399670c0fae3a299dcdf17f1f
Miles <Miles@MSI>
2018-04-17 11:18:48 +0100

    Reading More information from the text file

--- JukeboxMain/JukeboxMain/Form1.cs
+++ JukeboxMain/JukeboxMain/Form1.cs
@@ -19,19 +19,46 @@ namespace JukeboxMain
         string MediaFileDirectory = Directory.GetCurrentDirectory() + "\\Media\\";
         int NoOfLists;
 
+        string[] ListOne = new string[20];
+
+
+        
+
         private void Form1_Load(object sender, EventArgs e)
         {
             StreamReader myInputStream = File.OpenText(MediaFileDirectory + "Media.txt");
             string LineOfText;
-            int LineCounter = 0;
-            while((LineOfText = myInputStream.ReadLine()) != null)
-            { 
-                if(int.TryParse(LineOfText, out NoOfLists))
-                {
-                    NoOfLists = Convert.ToInt32(LineOfText);
-                    MessageBox.Show(NoOfLists.ToString());
-                }
+            LineOfText = myInputStream.ReadLine();
+            if (int.TryParse(LineOfText, out NoOfLists))
+            {
+                NoOfLists = Convert.ToInt32(LineOfText);
+                MessageBox.Show(NoOfLists.ToString());
             }
+
+
+            //List One Data Entry//
+            int Counter = 0;
+            ListOne[Counter] = myInputStream.ReadLine();
+            Counter++;
+            ListOne[Counter] = myInputStream.ReadLine();
+            Counter++;
+            for (int index = 0; index < Convert.ToInt32(ListOne[0]); index++)
+            {
+                ListOne[Counter] = myInputStream.ReadLine();
+                Counter++;
+            }
+
+            int test = 0;
+            while(test < 10)
+            {
+                MessageBox.Show(ListOne[test]);
+                test++;
+            }
+            
+            //while ((LineOfText = myInputStream.ReadLine()) != null)
+            //{
+           // }
+            
         }
     }
 }
